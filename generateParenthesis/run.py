7121d75c00d8a61f2ec07e56524a1122ee26d7da
root <vishwesh18119@iiitd.ac.in>
2020-07-30 15:28:06 +0000

    Updates some more

--- generateParenthesis/run.py
+++ generateParenthesis/run.py
@@ -3,7 +3,10 @@
 def generateParenthesis(N):
     if N == 0: return ['']
     ans = [] # watchvar ans
-    for c in range(N):
+    c = 0 # watchvar c
+    left = []  # watchvar left
+    right = [] # watchvar right
+    for c in range(N): 
         for left in generateParenthesis(c):
             for right in generateParenthesis(N-1-c):
                 ans.append('({}){}'.format(left, right))
